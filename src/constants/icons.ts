393532d248f69c334463404dc400ccd050e06a7a
DmitryLasuta <lasutadima01@gmail.com>
2024-04-14 00:10:52 +0300

    feat: organized import of icons for convenience

---
+++ src/constants/icons.ts
@@ -0,0 +1,10 @@
+import * as brandsIcons from '@fortawesome/free-brands-svg-icons';
+import * as regularIcons from '@fortawesome/free-regular-svg-icons';
+import * as solidIcons from '@fortawesome/free-solid-svg-icons';
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const icons: Record<string, any> = {
+  regular: regularIcons,
+  solid: solidIcons,
+  brands: brandsIcons,
+};
